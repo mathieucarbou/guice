3a2e9e3cdf92cd9c3963253ebbce92c7248240ba
mathieucarbou <mathieu.carbou@gmail.com>
2015-01-14 17:52:37 -0500

    support for field name injection for @Resource

--- extensions/jsr250/src/main/java/com/mycila/guice/ext/jsr250/Jsr250KeyProvider.java
+++ extensions/jsr250/src/main/java/com/mycila/guice/ext/jsr250/Jsr250KeyProvider.java
@@ -15,23 +15,47 @@
  */
 package com.mycila.guice.ext.jsr250;
 
+import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import com.mycila.guice.ext.injection.KeyProviderSkeleton;
 
 import javax.annotation.Resource;
+import javax.inject.Inject;
 import java.lang.reflect.Field;
 
 /**
  * @author Mathieu Carbou (mathieu.carbou@gmail.com)
  */
 class Jsr250KeyProvider extends KeyProviderSkeleton<Resource> {
+
+    @Inject Injector injector;
+
     @Override
     public Key<?> getKey(TypeLiteral<?> injectedType, Field injectedMember, Resource resourceAnnotation) {
         String name = resourceAnnotation.name();
-        return name.length() == 0 ?
-                super.getKey(injectedType, injectedMember, resourceAnnotation) :
-                Key.get(injectedType.getFieldType(injectedMember), Names.named(name));
+
+        if (name.length() != 0) {
+            // explicit key
+
+            // if a name is provided, it acts as a Named binding and this means we ask for a precise key
+            return Key.get(injectedType.getFieldType(injectedMember), Names.named(name));
+
+        } else {
+            // implicit key
+
+            // if no name given, try a combination with the field name
+            Key<?> implicitKey = Key.get(injectedType.getFieldType(injectedMember), Names.named(name));
+
+            if (injector.getExistingBinding(implicitKey) != null) {
+                return implicitKey;
+
+            } else {
+                // else create the find based on the field type (default behavior) - with optional existing binding annotations
+                return super.getKey(injectedType, injectedMember, resourceAnnotation);
+            }
+
+        }
     }
 }
